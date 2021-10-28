16f4b6cce855215aa92c2ec9bccd64a632d7d632
Juan Manuel Pajoy <jpajoyl@unal.edu.co>
2021-10-28 02:03:37 -0500

    add awesome reporter

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -25,27 +25,56 @@ describe('Buy a t-shirt', () => {
   const paymentStepPage: PaymentStepPage = new PaymentStepPage();
   const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
   const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-  it('then should be bought a t-shirt', async () => {
-    await browser.get('http://automationpractice.com/');
-    await menuContentPage.goToTShirtMenu();
-    await browser.wait(EC.elementToBeClickable(productListPage.getAddToCart()), 10000);
-    await productListPage.addTShirtCart();
+  describe('Open page', () => {
+    it('Open page in browser', async () => {
+      await browser.get('http://automationpractice.com/');
+    });
+  });
 
-    await browser.wait(EC.elementToBeClickable(productAddedModalPage.getProcedToCheckout()), 10000);
-    await productAddedModalPage.goToCheckOut();
-    await summaryStepPage.procedToCheckout();
-
-    await signInStepPage.sendCredentials('aperdomobo@gmail.com', 'WorkshopProtractor');
-    await signInStepPage.signIn();
-
-    await adressStepPage.goToCheckout();
-
-    await shippingStepPage.selectCheckbox();
-
-    await shippingStepPage.goToCheckout();
-    await paymentStepPage.continue();
-    await orderSummaryPage.confirmOrder();
-
-    await bankPaymentPage.changeText('Your order on My Store is complete.');
+  describe('Buy TShirt process', () => {
+    it('go to menu', async () => {
+      await menuContentPage.goToTShirtMenu();
+    });
+    browser.wait(EC.elementToBeClickable(productListPage.getAddToCart()), 10000);
+    it('add t-shirt to cart', async () => {
+      await productListPage.addTShirtCart();
+    });
+    browser.wait(EC.elementToBeClickable(productAddedModalPage.getProcedToCheckout()), 10000);
+    it('go to checkout', async () => {
+      await productAddedModalPage.goToCheckOut();
+    });
+    it('proced to checkout', async () => {
+      await summaryStepPage.procedToCheckout();
+    });
+  });
+  describe('App login', () => {
+    it('set credentials', async () => {
+      await signInStepPage.sendCredentials('aperdomobo@gmail.com', 'WorkshopProtractor');
+    });
+    it('login', async () => {
+      await signInStepPage.signIn();
+    });
+  });
+  describe('select adress', () => {
+    it('continue to payment', async () => {
+      await adressStepPage.goToCheckout();
+    });
+  });
+  describe('Bank payment', () => {
+    it('select checkbox', async () => {
+      await shippingStepPage.selectCheckbox();
+    });
+    it('go to final checkout', async () => {
+      await shippingStepPage.goToCheckout();
+    });
+    it('Continue to payment', async () => {
+      await paymentStepPage.continue();
+    });
+    it('Confirm order', async () => {
+      await orderSummaryPage.confirmOrder();
+    });
+    it('verify text', async () => {
+      await bankPaymentPage.changeText('Your order on My Store is complete.');
+    });
   });
 });
